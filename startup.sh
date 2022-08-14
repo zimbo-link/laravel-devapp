12242a86e610f650be04a8af4d63d38656221dc6
Trevor <trevorschuil@yahoo.com>
2022-08-14 23:20:38 +0200

    fix: start ups

---
+++ startup.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+chown -R www-data:www-data /srv/storage/
+
+#composer dumpautoload
+
+php  $APP_HOME/artisan config:cache
+php  $APP_HOME/artisan config:clear
+php  $APP_HOME/artisan view:clear
+php  $APP_HOME/artisan route:clear
