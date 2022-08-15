7ceffd191bf662258d758c77617248429eefe4da
Trevor <trevorschuil@yahoo.com>
2022-08-15 14:27:29 +0200

    fix: seed

--- startup.sh
+++ startup.sh
@@ -8,8 +8,7 @@ php  $APP_HOME/artisan config:cache
 php  $APP_HOME/artisan config:clear
 php  $APP_HOME/artisan view:clear
 php  $APP_HOME/artisan route:clear
-php  $APP_HOME/artisan db:seed --class=UsersTableSeeder
-php  $APP_HOME/artisan db:seed --class=InterestsTableSeeder
+php  $APP_HOME/artisan db:seed
 
 npm install
 npm run build
